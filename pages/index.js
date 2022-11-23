ef70c8ae384cd33cc700d6ec63648deb48f5f31c
Gnoompa <gnoompa@gmail.com>
2022-11-23 15:31:40 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>w3meta</title>
+        <meta name="description" content="web3 citizen meta profile" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>w3meta</h1>
+        <h2>web3 citizen meta profile</h2>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
